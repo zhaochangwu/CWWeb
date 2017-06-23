5209c2ccb8062e09839cfd048b77d1ed31635b95
zhaochangwu <changwuzhao@cienet.com.cn>
2017-06-23 09:24:47 +0800

    学习

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.write("<h1>This is a heading</h1>");
+// window.alert( "message" );
+(function myFunction()
+{
+document.write("糟糕！文档消失了。");
+})()
+function myFunction() {
+  window.open();
+}
